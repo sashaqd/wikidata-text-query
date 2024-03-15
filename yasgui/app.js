2bdc1bb241f63e61d689261649236229f6e5610e
sashaqd <106297572+sashaqd@users.noreply.github.com>
2024-03-15 12:37:50 +0400

    adding results to yasqe

--- yasgui/app.js
+++ yasgui/app.js
@@ -1,10 +1,135 @@
-// document.addEventListener("DOMContentLoaded", function() {
- 
-//     tab = yasgui.getTab();
-//     console.log(tab);
 
-// });
+// add text query box
+function addTextBox() {
 
+    console.log('Query box function called');
+
+    // Create the main container for the query box and buttons
+    const queryContainer = document.createElement('div');
+    queryContainer.classList.add('query-container');
+
+    // Create the textarea for entering queries
+    const queryBox = document.createElement('textarea');
+    queryBox.id = 'queryBox';
+    queryBox.setAttribute('placeholder', 'Enter your query here...');
+    queryBox.style.width = '100%';
+    queryBox.style.height = '150px'; // Adjust height as needed
+    queryBox.style.padding = '10px';
+    queryBox.style.margin = '10px 0';
+    queryBox.style.fontSize = '16px';
+    queryBox.style.fontFamily = 'Arial, sans-serif';
+    queryBox.style.boxSizing = 'border-box';
+    queryBox.style.border = '1px solid #ddd';
+
+    // container for the buttons
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+
+    // 'Run Query' button
+    const runQueryButton = document.createElement('button');
+    runQueryButton.id = 'runQuery';
+    runQueryButton.textContent = 'Run Query';
+
+    // 'Clear' button
+    const clearButton = document.createElement('button');
+    clearButton.id = 'clearQuery';
+    clearButton.textContent = 'Clear';
+
+    // Append buttons to the container
+    buttonContainer.appendChild(runQueryButton);
+    buttonContainer.appendChild(clearButton);
+
+    // Append the query box and button container to the query container
+    queryContainer.appendChild(queryBox);
+    queryContainer.appendChild(buttonContainer);
+
+    // Find the position where you want to insert the query container and insert it
+    const targetElement = document.querySelector('.yasqe'); 
+    if (targetElement) {
+        targetElement.parentNode.insertBefore(queryContainer, targetElement);
+    } else {
+        console.error("Target element '.yasqe' not found.");
+    }
+
+
+    var resultsContainer = document.createElement('div');
+    resultsContainer.classList.add('result-container');
+
+    const resultBox  = document.createElement('textarea');
+    resultBox.id = 'resultBox';
+    resultBox.setAttribute('placeholder', 'Enter your query here...');
+    resultBox.style.width = '100%';
+    resultBox.style.height = '50px'; // Adjust height as needed
+    resultBox.style.padding = '10px';
+    resultBox.style.margin = '10px 0';
+    resultBox.style.fontSize = '16px';
+    resultBox.style.fontFamily = 'Arial, sans-serif';
+    resultBox.style.boxSizing = 'border-box';
+    resultBox.style.border = '1px solid #ddd';
+
+
+    resultsContainer.appendChild(resultBox);
+    if (targetElement) {
+        targetElement.parentNode.insertBefore(resultsContainer, targetElement);
+    }
+    else {
+        console.error("Target element '.yasqe' not found.");
+    }
+}
+
+//running query
+function runQuery(query) {
+    const resultBox = document.getElementById('resultBox');
+        
+    resultBox.textContent = 'Loading';
+    resultBox.style.fontFamily = "Inter";
+    
+    let dotCount = 1;
+    let increasing = true;
+
+    const updateLoadingAnimation = function() {
+        // Update dot count
+        if (increasing) {
+            dotCount++;
+            if (dotCount >= 4) {
+                increasing = false;
+            }
+        } else {
+            dotCount--;
+            if (dotCount <= 1) {
+                increasing = true;
+            }
+        }
+        
+        resultBox.textContent = 'Loading' + '.'.repeat(dotCount);
+    };
+
+    const loadingAnimation = setInterval(updateLoadingAnimation, 500);
+
+    fetch('http://127.0.0.1:8000/run-query', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: query }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        clearInterval(loadingAnimation); // Stop the loading animation
+        // resultBox.textContent = data;
+        resultBox.textContent = ' ';
+        console.log('Success:', data);
+        var sparql_query = data['query'];
+   
+        var yasqe = yasgui.getTab().yasqe
+        yasqe.setValue(sparql_query);
+    })
+    .catch((error) => {
+        clearInterval(loadingAnimation); // Stop the loading animation
+        resultBox.textContent = 'Error occurred'; // Display error message
+        console.error('Error:', error);
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     
@@ -21,35 +146,24 @@ document.addEventListener('DOMContentLoaded', function() {
     closeTabDiv = document.querySelector('#tab-' + tabId + ' .closeTab');
     closeTabDiv.style.display = 'none';
 
+    //adding custom text box
+    addTextBox();
 
-    console.log('box function called');
-
-    // Create and insert the custom box before the yasqeDiv
-    const customBox = document.createElement('div');
-    customBox.classList.add('custom-box');
-    customBox.style.width = '1425px';
-    customBox.style.height = '300px';
-    customBox.style.padding = '10px';
-    customBox.style.marginLeft = '-10px';
+    //runnnig query
+    document.getElementById('runQuery').addEventListener('click', function() {
+        const query = document.getElementById('queryBox').value;
+        runQuery(query);
+    });
 
 
-    const textBox = document.createElement('input');
-    textBox.setAttribute('type', 'text');
-    textBox.setAttribute('placeholder', 'Enter text query');
-    textBox.style.width = '100%';
-    textBox.style.paddingLeft = '40px';
+    var clearButton = document.getElementById('clearQuery');
+    var queryBox = document.getElementById('queryBox');
 
-    textBox.style.fontSize = '19px';
-    textBox.style.fontFamily = 'Arial, sans-serif';
+    clearButton.addEventListener('click', function() {
+        queryBox.value = '';
+    });
 
-    textBox.style.height = '100%';
-    textBox.style.boxSizing = 'border-box';
-    textBox.style.border = '1px solid #ddd';
-    
-    customBox.appendChild(textBox);
-    
-    const yasqeDiv = document.querySelector('.yasqe');
-    yasqeDiv.parentNode.insertBefore(customBox, yasqeDiv);
-   
 });
+
+
 
---
+++ yasgui/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Any, Dict
+from fastapi.middleware.cors import CORSMiddleware
+from openai import OpenAI
+
+origins = [
+    "http://127.0.0.1:5500", 
+    "http://localhost:8000",  
+]
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Query(BaseModel):
+    query: str
+
+@app.post("/run-query")
+async def run_query(query: Query) -> Any:
+    
+    api_key = "" # add key here
+    client = OpenAI(api_key=api_key)
+    text_query = query.query
+
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {
+            "role": "system",
+            "content": "Write an appropriate SPARQL Query to retrieve the following information from Wikidata: " + text_query
+            }
+        ]
+    )
+
+    sparql_query = completion.choices[0].message.content
+
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {
+            "role": "system",
+            "content": "From the following text insert the just the SPARQL Query in \"\"\" insert query here \"\"\": " + str(sparql_query)
+            }
+        ]
+    )
+
+    final_query = completion.choices[0].message.content
+    final_query = final_query.split('\"\"\"')[1]
+    return {"query": final_query}
